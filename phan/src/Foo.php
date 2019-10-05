0687151e213ec0a55338ade2d8cde9c5fa4a3f4d
tubutubu_mustard <tubutubu.mustard@gmail.com>
2019-10-05 15:32:50 +0900

    docker+phan

---
+++ phan/src/Foo.php
@@ -0,0 +1,13 @@
+<?php
+
+class Foo
+{
+    public function bar()
+    {
+        $one   = 1;
+        $two   = 2;
+        $three = 3;
+        var_dum($one);
+        var_dump($one + $two);
+    }
+}
